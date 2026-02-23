66bbdbd64fc00e28cd731b30d6c486033a67130b
yeshkochar <kocharyesh@yahoo.com>
2026-02-23 15:07:30 +0530

    usecase1

--- src/PallindromeCheckerApp.java
+++ src/PallindromeCheckerApp.java
@@ -1,3 +1,8 @@
 public class PallindromeCheckerApp {
+    static void main(String[] args) {
+        System.out.println("welcome");
+        System.out.println("Version: 1.0");
+        System.out.println("System initialized successfully");
+    }
 
 }
